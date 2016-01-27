75ca48453a522443040147e0994e4cf57028e528
Michael Wiseley <wiseley@mail.usf.edu>
2016-01-27 18:16:50 -0500

    Initial commit

---
+++ app/src/main/java/io/propty/propty/SplashActivity.java
@@ -0,0 +1,20 @@
+package io.propty.propty;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Michael on 1/27/2016.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
