67008c3850b332aef79bfd2897d1ef75f176bc9a
Michael Wiseley <wiseley@mail.usf.edu>
2016-01-28 13:19:44 -0500

    fixed duplicate code in login activity

--- app/src/main/java/io/propty/propty/LoginActivity.java
+++ app/src/main/java/io/propty/propty/LoginActivity.java
@@ -138,23 +138,5 @@ package io.propty.propty;
             }
              boolean valid = true;
 
-        String email = _emailText.getText().toString();
-        String password = _passwordText.getText().toString();
-
-        if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-            _emailText.setError("enter a valid email address");
-            valid = false;
-        } else {
-            _emailText.setError(null);
-        }
-
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-            _passwordText.setError("between 4 and 10 alphanumeric characters");
-            valid = false;
-        } else {
-            _passwordText.setError(null);
-        }
-
-        return valid;
     }
 
