7ce6101d08949b91556b1831adaf26853f9be4ff
Michael Wiseley <wiseley@mail.usf.edu>
2016-01-28 14:35:12 -0500

    Implementing facebook login

--- app/src/main/java/io/propty/propty/LoginActivity.java
+++ app/src/main/java/io/propty/propty/LoginActivity.java
@@ -147,26 +147,7 @@ package io.propty.propty;
             public boolean validate() {
                 boolean valid = true;
 
-                String email = _emailText.getText().toString();
-                String password = _passwordText.getText().toString();
-
-                if (email.isEmpty() || !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    _emailText.setError("enter a valid email address");
-                    valid = false;
-                } else {
-                    _emailText.setError(null);
-                }
-
-                if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
-                    _passwordText.setError("between 4 and 10 alphanumeric characters");
-                    valid = false;
-                } else {
-                    _passwordText.setError(null);
-                }
-
-                return valid;
             }
-             boolean valid = true;
 
     }
 
