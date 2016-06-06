b5023a9352148c86f64961f03b437fb3013c93d2
Tom Kelly <TomK0874@gmail.com>
2016-06-06 19:39:24 -0400

    attempted manual merge of Ryan's branch

---
+++ app/src/main/java/io/propty/propty/RealtorExpandableAdapter.java
@@ -0,0 +1,215 @@
+package io.propty.propty;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.github.siyamed.shapeimageview.CircularImageView;
+
+import java.util.HashMap;
+import java.util.List;
+
+//TODO: IMPLEMENT IMAGES TO BE DISPLAYED WITH REALTOR NAMES
+
+/*
+This is a custom adapter for the Expandable ListView being used for
+the RealtorList Activity. It uses an ArrayList for the realtor names,
+and a HashMap for the realtor info/descriptions. An array of ratings
+are also added to implement a rating bar in a child view of each
+group, along with a button that picks the realtor currently expanded
+*/
+public class RealtorExpandableAdapter extends BaseExpandableListAdapter {
+
+    private Context mContext;
+    private List<String> mParent;
+    private HashMap<String, String> mChild;
+    private String[] mRatings;
+    private int[] imageIds;
+
+    private GroupViewHolder mGroupViewHolder;
+    private ChildViewHolder mChildViewHolder;
+
+    //CONSTRUCTOR
+    public RealtorExpandableAdapter(Context context, List<String> list,
+                                    HashMap<String, String> hashMap, String[] strArray, int[] images) {
+
+        //set all arguments to member variables
+        mContext = context;
+        mParent = list;
+        mChild = hashMap;
+        mRatings = strArray;
+        imageIds = images;
+
+    }
+
+    @Override
+    public int getGroupCount() {
+
+        return mParent.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        //Note: A hard code of 1 is used, because only
+        //one "child" is used for each parent, a string.
+        return 1;
+
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+
+        return mParent.get(groupPosition);
+
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+
+        return mChild.get(mParent.get(groupPosition));
+
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+
+        //get a string for the name of the realtor
+        String realtor_name = (String) getGroup(groupPosition);
+        int realtor_image = imageIds[groupPosition];
+
+        //if the view is brand new
+        if(convertView == null) {
+            //create a new LayoutInflator, and set it to inflate the brand new view
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.realtor_parent, parent, false);
+            //create a new ViewHolder for the group, and set it to the proper xml id
+            mGroupViewHolder = new GroupViewHolder();
+            mGroupViewHolder.mGroupText = (TextView) convertView.findViewById(R.id.realtor_parent_textview);
+            mGroupViewHolder.mGroupImage = (CircularImageView) convertView.findViewById(R.id.realtor_circle_image);
+            //give the view a tag for future use
+            convertView.setTag(mGroupViewHolder);
+
+        }
+        else {
+            //make the view from the previous tag used
+            mGroupViewHolder = (GroupViewHolder) convertView.getTag();
+        }
+        //set the TextView in the parent to the name of the realtor, and make it BOLD
+        mGroupViewHolder.mGroupText.setTypeface(null, Typeface.BOLD);
+        mGroupViewHolder.mGroupText.setText(realtor_name);
+        mGroupViewHolder.mGroupImage.setImageResource(realtor_image);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(final int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        //get a string of the info/description of the realtor
+        String realtor_info = (String) getChild(groupPosition, childPosition);
+        //get the string "value" of the rating of the current realtor
+        String rating = mRatings[groupPosition];
+
+        //if the view is brand new
+        if(convertView == null) {
+            //create a new LayoutInflator, and set it to inflate the new view
+            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.realtor_child, parent, false);
+            //create a new ViewHolder for the child, and set it to the proper xml id
+            mChildViewHolder = new ChildViewHolder();
+            //set the TextView, Button, and RatingBar views from the child to the proper xml id's
+            mChildViewHolder.mChildText = (TextView) convertView.findViewById(R.id.realtor_child_textview);
+            mChildViewHolder.mChildButton = (Button) convertView.findViewById(R.id.realtor_child_button);
+            mChildViewHolder.mRatingBar = (RatingBar) convertView.findViewById(R.id.realtor_child_ratingbar);
+            //make the RatingBar NOT able to be changed by the user
+            mChildViewHolder.mRatingBar.setIsIndicator(true);
+
+            //give the view a tag for future use
+            convertView.setTag(R.layout.realtor_child, mChildViewHolder);
+
+        }
+        else {
+            //make a view from the previous tag used
+            mChildViewHolder = (ChildViewHolder) convertView.getTag(R.layout.realtor_child);
+        }
+        //set the TextView to the string value of the realtor info/description
+        mChildViewHolder.mChildText.setText(realtor_info);
+        //set the rating bar value to the float version of the rating string
+        mChildViewHolder.mRatingBar.setRating(Float.parseFloat(rating));
+
+        //create a new listener for when the user clicks the "Pick Realtor" button
+        mChildViewHolder.mChildButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //get the string of the current realtor name in the group the button belongs to
+                String pickedRealtor = (String) getGroup(groupPosition);
+                //get the TextView that displays the current realtor, in the RealtorList Activity
+                TextView tv = (TextView) ((Activity)mContext).findViewById(R.id.picked_realtor);
+                //set the textview to the color green, with the new value
+                tv.setTextColor(Color.GREEN);
+                tv.setText("Current Realtor: " + pickedRealtor);
+                //make a Toast for confirmation of the picked realtor
+                Toast.makeText(mContext, pickedRealtor + " is now your realtor", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+
+    /*
+    a ViewHolder for all View variables
+    in the Parents of the Expandable ListView
+    */
+    // TODO: add an ImageView to be used
+    public final class GroupViewHolder {
+
+        TextView mGroupText;
+        ImageView mGroupImage;
+    }
+
+    /*
+    a ViewHOlder for all View variables
+    in the Child of each Parent in the
+    Expandable ListView
+    */
+    public final class ChildViewHolder {
+
+        TextView mChildText;
+        Button mChildButton;
+        RatingBar mRatingBar;
+
+    }
+
+
+}
--- app/src/main/java/io/propty/propty/SwipeCardActivity.java
+++ app/src/main/java/io/propty/propty/SwipeCardActivity.java
@@ -1,8 +1,8 @@
 package io.propty.propty;
 
-import android.content.Context;
-//import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -21,61 +21,35 @@ import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import butterknife.OnClick;
-//import com.amazonaws.auth.CognitoCachingCredentialsProvider;
-//import com.amazonaws.regions.Region;
-//import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
-//import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.lorentzos.flingswipe.SwipeFlingAdapterView;
-
 import java.io.*;
 import java.util.ArrayList;
 
 /**
  * Swipe Card Main Activity
- * This is Ryan's rendition of the Swipe Card Activity with a navigation drawer
- * added on the right side.
- *
  * Called when the user wants to see local property listings in a swipe-card format
  * Uses an ArrayList of SwipeCard objects to populate the list
  * Also
  */
 public class SwipeCardActivity extends AppCompatActivity {
 
-    //private variables that Ryan added for DrawerLayout
     private DrawerLayout mDrawerLayout;
     private TextView mMinTextView;
     private TextView mMaxTextView;
     private RadioButton mCurrentZipButton;
     private RadioButton mOtherZipButton;
-    //end of Ryan's variables
 
     private ArrayList<SwipeCard> sc;
     private ArrayList<String> al;
     private ArrayAdapter<String> arrayAdapter;
     private Toolbar toolbar;
 
-    private static final int MAX_CARDS_IN_STACK = 20;
-    private static final int MIN_CARDS_IN_STACK = 6;
-    private static final int MAX_RENDERED_CARDS = 4;
-    private int current_card = 0;
-    private int idx = 1;
-//    private final String bucketName = "propty.mobilepull";
-//    private String key = "samples/sample";
-
-    @InjectView(R.id.frame) SwipeFlingAdapterView flingContainer;
+    int current_card = 0;
+    int idx = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.drawer_preferences);
-        ButterKnife.inject(this);
 
         //TODO: Get logo centered with menu items present
         //setting up toolbar...
@@ -84,89 +58,6 @@ public class SwipeCardActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         toolbar.setLogo(R.drawable.proptydog2);
         toolbar.setLogoDescription("logo");
-
-        sc = new ArrayList<>();
-        al = new ArrayList<>();
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.swipecard, R.id.cardText, al);
-
-        // Initialize the Amazon Cognito credentials provider
-//        CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
-//                getApplicationContext(),
-//                "us-east-1:bdd94561-cf5e-4693-995e-f4039ad7e234", // Identity Pool ID
-//                Regions.US_EAST_1 // Region
-//        );
-
-        // Create an S3 client
-//        final AmazonS3 s3 = new AmazonS3Client(credentialsProvider);
-
-        // Set the region of your S3 bucket
-//        s3.setRegion(Region.getRegion(Regions.US_EAST_1));
-
-        // Populate Swipe Cards from the S3 bucket
-//        pullDataFromBucket(s3, bucketName, key);
-
-        // These are just examples for testing purposes.  TODO: Delete these before launch!
-        sc.add(new SwipeCard("Property 1", "", 2, 2, 0, 0, 0, 2000, "sq ft", 960.00d, "condo", "", 1));
-        sc.add(new SwipeCard("Property 2", "", 3, 2, 0, 1, 0, 3500, "sq ft", 2250.00d, "house", "", 2));
-        sc.add(new SwipeCard("Property 3", "", 1, 1, 0, 0, 0, 1200, "sq ft", 625.50d, "apartment", "", 3));
-        sc.add(new SwipeCard("Property 4", "", 1, 1, 0, 1, 0, 1500, "sq ft", 500.01d, "duplex", "", 4));
-        sc.add(new SwipeCard("Property 5", "", 1, 0, 0, 1, 0, 150, "sq ft", 99.99d, "closet", "", 5));
-
-        flingContainer.setAdapter(arrayAdapter);
-        flingContainer.setMaxVisible(MAX_RENDERED_CARDS);
-        flingContainer.setMinStackInAdapter(MIN_CARDS_IN_STACK);
-        flingContainer.setFlingListener(new SwipeFlingAdapterView.onFlingListener() {
-            @Override
-            public void removeFirstObjectInAdapter() {
-                // this is the simplest way to delete an object from the Adapter (/AdapterView)
-                Log.d("LIST", "removed object!");
-                al.remove(0);
-                current_card = (current_card + 1) % sc.size();
-                arrayAdapter.notifyDataSetChanged();
-            }
-
-            @Override
-            public void onLeftCardExit(Object dataObject) {
-                //Do something on the left!
-                //You also have access to the original object.
-                //If you want to use it just cast it (String) dataObject
-                makeToast(getApplicationContext(), "Dislike!");
-            }
-
-            @Override
-            public void onRightCardExit(Object dataObject) {
-                makeToast(getApplicationContext(), "Like!");
-            }
-
-            @Override
-            public void onAdapterAboutToEmpty(int itemsInAdapter) {
-//                pullDataFromBucket(s3, bucketName, key);
-                updateCardArray(sc.size() * 4);
-                Log.d("LIST", "notified");
-            }
-
-            @Override
-            public void onScroll(float scrollProgressPercent) {
-                View view = flingContainer.getSelectedView();
-                view.findViewById(R.id.item_swipe_right_indicator).setAlpha(scrollProgressPercent < 0 ? -scrollProgressPercent : 0);
-                view.findViewById(R.id.item_swipe_left_indicator).setAlpha(scrollProgressPercent > 0 ? scrollProgressPercent : 0);
-            }
-        });
-
-        // Optionally add an OnItemClickListener
-        flingContainer.setOnItemClickListener(new SwipeFlingAdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClicked(int itemPosition, Object dataObject) {
-                makeToast(getApplicationContext(), "Clicked!");
-            }
-        });
-
-        updateCardArray(sc.size() * 4);
-
-        //////////////////////////////////////////////////////////
-        //Ryan's section of implementing DrawerLayout
-        //and preferences menu
-        /////////////////////////////////////////////////////////
 
         //initialize DrawerLayout and set an event listener
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -200,16 +91,13 @@ public class SwipeCardActivity extends AppCompatActivity {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 //closes the drawer if the user taps the area outside of it
-                if(mDrawerLayout.isDrawerOpen(GravityCompat.END)) {
+                if (mDrawerLayout.isDrawerOpen(GravityCompat.END)) {
                     mDrawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
-//                    mDrawerLayout.closeDrawer(GravityCompat.END);
                 }
 
                 return false;
             }
         });
-
-
 
         //Set up the Bedroom Number Picker with minimum and maximum values
         NumberPicker mBedroomPicker = (NumberPicker) findViewById(R.id.num_bedroom);
@@ -248,7 +136,7 @@ public class SwipeCardActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                if(isChecked) {
+                if (isChecked) {
                     //get id of checked button
                     int id = buttonView.getId();
 
@@ -262,7 +150,7 @@ public class SwipeCardActivity extends AppCompatActivity {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                if(isChecked) {
+                if (isChecked) {
                     int id = buttonView.getId();
 
                     //uncheck other radio buttons
@@ -344,175 +232,23 @@ public class SwipeCardActivity extends AppCompatActivity {
             }
         });
 
+        //START FRAGMENT TRANSACTION
+        SwipeCardFragment swipeCardFragment = new SwipeCardFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, swipeCardFragment).commit();
 
-
-
-
-
+        //END OF ONCREATE METHOD
     }
-
-    static void makeToast(Context ctx, String s) {
-        Toast.makeText(ctx, s, Toast.LENGTH_SHORT).show();
-    }
-
-    /**
-     * Trigger the right event manually.
-     */
-    @OnClick(R.id.right)
-    public void right() { flingContainer.getTopCardListener().selectRight(); }
-
-    /**
-     * Trigger the left event manually.
-     */
-    @OnClick(R.id.left)
-    public void left() { flingContainer.getTopCardListener().selectLeft(); }
-
-    /**
-     * Pull data objects from the Amazon S3 bucket until the card stack is full
-     * Calls generateCardFromData() and updateCardArray()
-     * @param s3 An AmazonS3Client object
-     * @param bucketName The name of the S3 bucket
-     * @param key The object key
-     */
-    protected void pullDataFromBucket(AmazonS3 s3, String bucketName, String key) {
-        String key_0;
-        int cardsInStack = sc.size();
-        while (cardsInStack < MAX_CARDS_IN_STACK) {
-            if (idx < 10) {
-                key_0 = key + "0" + Integer.toString(idx) + ".txt";
-            }
-            else {
-                key_0 = key + Integer.toString(idx) + ".txt";
-            }
-            idx++;
-            try {
-                S3Object object = s3.getObject(new GetObjectRequest(bucketName, key_0));
-                InputStream objectData = object.getObjectContent();
-                try {
-                    sc.add(generateCardFromData(objectData));
-                } finally {
-                    try {
-                        objectData.close();
-                    } catch (Throwable ignore) {}
-                }
-            } catch (IOException e) { e.printStackTrace(); }
-            cardsInStack = sc.size();
-        }
-        updateCardArray(sc.size());
-    }
-
-    /**
-     * Parses the provided S3 bucket object data and generates a single SwipeCard object
-     * @param objectData An InputStream object pulled from the S3 bucket
-     * @return Returns a new SwipeCard object containing the data parsed from objectData
-     * @throws IOException
-     */
-    protected SwipeCard generateCardFromData(InputStream objectData) throws IOException {
-        // Default to null values
-        String desc = "NULL";
-        String listId = "NULL";
-        String areaUnits = "NULL";
-        String type = "NULL";
-        String subType = "NULL";
-        double price = -1d;
-        int beds = -1;
-        int bathsFull = -1;
-        int baths3q = -1;
-        int bathsHalf = -1;
-        int baths1q = -1;
-        int area = -1;
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(objectData));
-        String str;
-
-        for (int i = 0; i < 12; i++) {
-            if ((str = reader.readLine()) != null) {
-                switch (i) {
-                    case 0:
-                        desc = str;
-                        break;
-                    case 1:
-                        listId = str;
-                        break;
-                    case 2:
-                        beds = Integer.parseInt(str);
-                        break;
-                    case 3:
-                        bathsFull = Integer.parseInt(str);
-                        break;
-                    case 4:
-                        baths3q = Integer.parseInt(str);
-                        break;
-                    case 5:
-                        bathsHalf = Integer.parseInt(str);
-                        break;
-                    case 6:
-                        baths1q = Integer.parseInt(str);
-                        break;
-                    case 7:
-                        area = Integer.parseInt(str);
-                        break;
-                    case 8:
-                        areaUnits = str;
-                        break;
-                    case 9:
-                        if (str.charAt(0) == '$')
-                            str = str.substring(1);
-                        price = Double.parseDouble(str);
-                        break;
-                    case 10:
-                        type = str;
-                        break;
-                    case 11:
-                        subType = str;
-                        break;
-                }
-            }
-            else {
-                throw new IOException("Error occurred while retrieving data from server");
-            }
-        }
-
-        int img = 0;
-
-        return new SwipeCard(desc, listId, beds, bathsFull, baths3q, bathsHalf, baths1q,
-                area, areaUnits, price, type, subType, img);
-    }
-
-    /**
-     * Converts SwipeCards from the ArrayList of SwipeCards (sc) into formatted Strings, then
-     * adds the specified number of converted SwipeCards to the ArrayList of formatted Strings (al).
-     * This allows the activity to properly display the formatted SwipeCard data.
-     *
-     * @param number_to_add The number of SwipeCards to add to the stack
-     */
-    protected void updateCardArray(int number_to_add) {
-        for (int i = 0; i < number_to_add; i++) {
-            SwipeCard card = sc.get(i % sc.size());
-            al.add(card.getDescFormatted() +
-                    card.getBedsFormatted() +
-                    card.getBathsFormatted() +
-                    card.getAreaFormatted() +
-                    card.getPriceFormatted() +
-                    card.getTypeFormatted());
-        }
-        arrayAdapter.notifyDataSetChanged();
-    }
-
-    ////////////////////////////////////////////////////
-    //Ryan's section overridden methods
-    //specifically for the drawer and action bar icon
-    ///////////////////////////////////////////////////
 
     //close drawer when back button is pressed
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
-        if(drawer.isDrawerOpen(GravityCompat.END)) {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
             drawer.closeDrawer(GravityCompat.END);
-        }
-        else {
+        } else {
             super.onBackPressed();
         }
 
@@ -544,37 +280,4 @@ public class SwipeCardActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-
-
-
-
-//    protected void renderCardBackground() {
-//        if (flingContainer.getChildAt(0) == null) return;
-//        View view = flingContainer.getChildAt(0).findViewById(R.id.cardText);
-//        switch (current_card) {
-//            case 0:
-//                view.setBackgroundResource(R.drawable.house_0);
-//                break;
-//            case 1:
-//                view.setBackgroundResource(R.drawable.house_1);
-//                break;
-//            case 2:
-//                view.setBackgroundResource(R.drawable.house_2);
-//                break;
-//            case 3:
-//                view.setBackgroundResource(R.drawable.house_3);
-//                break;
-//            case 4:
-//                view.setBackgroundResource(R.drawable.house_4);
-//                break;
-//            default:
-//                break;
-//        }
-//    }
-
-//    private class downloadFromBucket extends AsyncTask<S3Object, Void, Void> {
-//        protected S3Object doInBackground(AmazonS3 s3, String key_0) {
-//            return s3.getObject(new GetObjectRequest(bucketName, key_0));
-//        }
-//    }
 }
