95d0a1a2605ecf12cb5206025ddb19065be427ba
mcwiseley <wiseley@chainring.net>
2016-07-06 13:14:35 -0400

    Derp

--- app/src/main/java/io/propty/propty/MyGcmPushReciever.java
+++ app/src/main/java/io/propty/propty/MyGcmPushReciever.java
@@ -60,6 +60,67 @@ public class MyGcmPushReceiver extends GcmListenerService {
             }
         }
     }
+    /**
+     * Processing user specific push message
+     * It will be displayed with / without image in push notification tray
+     * */
+    private void processUserMessage(String title, boolean isBackground, String data) {
+        if (!isBackground) {
+
+            try {
+                JSONObject datObj = new JSONObject(data);
+
+                String imageUrl = datObj.getString("image");
+
+                JSONObject mObj = datObj.getJSONObject("message");
+                Message message = new Message();
+                message.setMessage(mObj.getString("message"));
+                message.setId(mObj.getString("message_id"));
+                message.setCreatedAt(mObj.getString("created_at"));
+
+                JSONObject uObj = datObj.getJSONObject("user");
+                User user = new User();
+                user.setId(uObj.getString("user_id"));
+                user.setEmail(uObj.getString("email"));
+                user.setName(uObj.getString("name"));
+                message.setUser(user);
+
+                // verifying whether the app is in background or foreground
+                if (!NotificationUtils.isAppIsInBackground(getApplicationContext())) {
+
+                    // app is in foreground, broadcast the push message
+                    Intent pushNotification = new Intent(Config.PUSH_NOTIFICATION);
+                    pushNotification.putExtra("type", Config.PUSH_TYPE_USER);
+                    pushNotification.putExtra("message", message);
+                    LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+
+                    // play notification sound
+                    NotificationUtils notificationUtils = new NotificationUtils();
+                    notificationUtils.playNotificationSound();
+                } else {
+
+                    // app is in background. show the message in notification try
+                    Intent resultIntent = new Intent(getApplicationContext(), MainActivity.class);
+
+                    // check for push notification image attachment
+                    if (TextUtils.isEmpty(imageUrl)) {
+                        showNotificationMessage(getApplicationContext(), title, user.getName() + " : " + message.getMessage(), message.getCreatedAt(), resultIntent);
+                    } else {
+                        // push notification contains image
+                        // show it with the image
+                        showNotificationMessageWithBigImage(getApplicationContext(), title, message.getMessage(), message.getCreatedAt(), resultIntent, imageUrl);
+                    }
+                }
+            } catch (JSONException e) {
+                Log.e(TAG, "json parsing error: " + e.getMessage());
+                Toast.makeText(getApplicationContext(), "Json parse error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            // the push notification is silent, may be other operations needed
+            // like inserting it in to SQLite
+        }
+    }
 
     /**
      * Showing notification with text only
